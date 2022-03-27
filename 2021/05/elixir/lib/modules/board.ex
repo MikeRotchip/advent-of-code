27edb677a9da9f37d405481e9229aa2b4b7f2083
MikeRotchip <p3tr.kada@gmail.com>
2022-03-27 22:00:00 +0200

    test commit

--- 2021/05/elixir/lib/modules/board.ex
+++ 2021/05/elixir/lib/modules/board.ex
@@ -1,31 +1,32 @@
 defmodule Board do
-
   @board_size 999
 
   def new() do
-    for x <- 0 .. @board_size, y <- 0 .. @board_size, into: %{}, do: {format_key(%{x: x, y: y}), 0}
+    for x <- 0..@board_size, y <- 0..@board_size, into: %{}, do: {format_key(%{x: x, y: y}), 0}
   end
 
   def add_line(map, %{x1: x1, y1: y1, x2: x2, y2: y2}) do
-    points = for x <- x1 .. x2, y <- y1 .. y2, do: %{:x => x, :y => y}
+    points = for x <- x1..x2, y <- y1..y2, do: %{:x => x, :y => y}
     add_points(map, points)
   end
 
   def get_crossings(map) do
-    get_all_cords() |>
-    Enum.reduce(0,
+    get_all_cords()
+    |> Enum.reduce(
+      0,
       fn cords, x ->
         if get(map, cords) > 1 do
-            x + 1
-          else
-            x
+          x + 1
+        else
+          x
         end
-    end)
+      end
+    )
   end
 
   defp add_points(map, [head | tail]) do
-    increment_rank(map, head) |>
-    add_points(tail)
+    increment_rank(map, head)
+    |> add_points(tail)
   end
 
   defp add_points(map, []) do
@@ -46,11 +47,11 @@ defmodule Board do
     %{map | format_key(cords) => val}
   end
 
-  defp format_key (%{x: x, y: y}) do
+  defp format_key(%{x: x, y: y}) do
     ~s"#{x}-#{y}"
   end
 
   defp get_all_cords() do
-    for x <- 0 .. @board_size, y <- 0 .. @board_size, do: %{x: x, y: y}
+    for x <- 0..@board_size, y <- 0..@board_size, do: %{x: x, y: y}
   end
 end
--- 2021/05/elixir/lib/modules/parser.ex
+++ 2021/05/elixir/lib/modules/parser.ex
@@ -1,5 +1,4 @@
 defmodule Parser do
-
   def get_line(input) do
     [line_start, line_end] = String.split(input, " -> ")
 
@@ -9,11 +8,11 @@ defmodule Parser do
     %{x1: x1, x2: x2, y1: y1, y2: y2}
   end
 
-  def is_not_diagonal (%{x1: x1, x2: x2, y1: y1, y2: y2}) do
+  def is_not_diagonal(%{x1: x1, x2: x2, y1: y1, y2: y2}) do
     x1 == x2 || y1 == y2
   end
 
-  defp get_cords (input) do
+  defp get_cords(input) do
     String.split(input, ",") |> Enum.map(fn x -> String.to_integer(x) end)
   end
 end
